36282ca2e0229847d957849a84d77bd8322b34f5
Rahul <gu.rahu1100@gmail.com>
2026-09-21 00:10:51 +0530

    working on new dataset

--- icd10Download.py
+++ icd10Download.py
@@ -1,10 +1,14 @@
-import kagglehub
-import shutil
+# import kagglehub
+# import shutil
 from pathlib import Path
 
 # Download latest version
-path = kagglehub.dataset_download("arashnic/icd10-codes-and-descriptions")
+# path = kagglehub.dataset_download("arashnic/icd10-codes-and-descriptions")
 destination = Path(__file__).resolve().parent
-shutil.move(path, destination)
+# shutil.move(path, destination)
 
-print("Path to dataset files:", destination / Path(path).name)
+# print("Path to dataset files:", destination / Path(path).name)
+
+from datasets import load_dataset
+
+ds = load_dataset("FiscaAI/synth-ehr-icd10cm-prompt", cache_dir=destination)
